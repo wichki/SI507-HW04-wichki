c31a5e1fae285f854a81129eae14a57c8ee3eafe
William Kim <wichki@umich.edu>
2018-02-04 14:25:13 -0500

    Person A, Part 2

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,10 @@
-response = input("What is your question?: ")
+while True:
+
+    response = input("What is your question?: ")
+
+    if "?" in response:
+        print(random.choice(validresponses))
+    elif response == "quit":
+        break
+    else:
+        print("I'm sorry, I can only answer questions.")
